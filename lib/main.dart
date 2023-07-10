3633566ed67063d2aa05690964bdc63dc713e7e7
kavi-24 <--replace-all>
2023-07-11 01:30:52 +0530

    Changes

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       onGenerateRoute: (settings) => generateRoute(settings),
-      home: AuthScreen(),
+      home: const AuthScreen(),
     );
   }
 }
